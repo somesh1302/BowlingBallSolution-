36f2431c0769ff1ed9341df677bbada1ad858f0a
Somesh <gavane.somesh@gmail.com>
2021-12-17 19:18:04 +0530

    balling ball problem solution

---
+++ bowling-ball/BowlingBall.Tests/GameFixture.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using BowlingBall;// added bowling ball refrence
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit;
+using NUnit.Framework;
+
+namespace BowlingBall.Tests
+{
+    [TestFixture()]
+    public class GameFixture
+    {
+        Game game;
+
+        [SetUp]
+        public void SetUPBallingBallGame()
+        {
+            game= new Game();
+        }
+
+        public void SameScoreInManyRolls(int rolls, int pins)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                game.Roll(pins);
+            }
+        }
+
+        [Test]
+        public void Gutter_game_score_should_be_zero_test()
+        {
+            SameScoreInManyRolls(20,0);
+            Assert.That(game.GetScore(),Is.EqualTo(0));
+        }
+        [Test]
+        public void TestCaseInProblem()
+        {
+            game.Roll(10);
+            game.Roll(9); game.Roll(1);
+            game.Roll(5); game.Roll(5);
+            game.Roll(7); game.Roll(2);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(9); game.Roll(0);
+            game.Roll(8); game.Roll(2);
+            game.Roll(9); game.Roll(1); game.Roll(10);
+
+            Assert.That(game.GetScore(), Is.EqualTo(187));
+        }
+        [Test]
+        public void Gutter_game_score_one_ineveryroll()
+        {
+            SameScoreInManyRolls(20, 1);
+
+            Assert.That(game.GetScore(), Is.EqualTo(20));
+        }
+        [Test]
+        public void FirstFrameSpareThenOneInEveryFrame()
+        {
+            game.Roll(9);
+            game.Roll(1);
+            SameScoreInManyRolls(18, 1);
+            Assert.That(game.GetScore(), Is.EqualTo(29));
+        }
+        [Test]
+        public void SparesEveryFrame()
+        {
+           for(int i=0;i<10;i++)
+            {
+                game.Roll(9);
+                game.Roll(1);
+            }
+            game.Roll(9);
+            Assert.That(game.GetScore(), Is.EqualTo(190));
+        }
+        [Test]
+        public void StrikeInEveryFrame()
+        {
+
+            SameScoreInManyRolls(12, 10);
+            Assert.That(game.GetScore(), Is.EqualTo(300));
+        }
+
+
+    }
+}
---
+++ bowling-ball/BowlingBall/Game.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingBall
+{
+    public class Game
+    {
+        int[] pinFallsAginstRolls = new int[21];
+        int rollsCounter;
+        public void Roll(int pins)
+        {
+            pinFallsAginstRolls[rollsCounter] = pins;
+            rollsCounter++;
+        }
+
+        public int GetScore()
+        {
+            int score = 0;
+            int i = 0;
+            for(int frame=0;frame<10;frame++)
+            {
+                if (pinFallsAginstRolls[i]==10)// strike  condition
+                {
+                    score += 10 + pinFallsAginstRolls[i + 1] + pinFallsAginstRolls[i + 2];
+                    
+                    i = i + 1;
+                }
+                else if (pinFallsAginstRolls[i]+ pinFallsAginstRolls[i+1]==10)// spare condition
+                {
+                    score += 10 + pinFallsAginstRolls[i + 2];
+                    i = i + 2;
+                }
+                else
+                {
+                    score += pinFallsAginstRolls[i] + pinFallsAginstRolls[i+1];
+                    i = i + 2;
+
+                }
+            }
+            return score;
+        }
+    }
+}
